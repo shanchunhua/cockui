cf8b918ab6421235e11c58c54569a01d629fbfc1
shanch <shanch@cn.ibm.com>
2017-06-03 11:53:05 +0800

    0602

--- pages/henrental/index.js
+++ pages/henrental/index.js
@@ -1,27 +1,36 @@
-// pages/leather/index.js
-var app = getApp()
-var customerBoxService = require('../../service/customerBox.js');
+// pages/rawegg/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: []
+    order: {
+      quantity: 1
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var self = this;
-    customerBoxService.loadCustomerBoxItems({ id: app.globalData.userInfo.id }).then(function (res) {
-      self.setData({
-        list: res.data
-      });
+
+  },
+  plus: function () {
+    this.data.order.quantity = this.data.order.quantity + 1;
+    this.setData({
+      order: this.data.order
     })
   },
+  minus: function () {
+    if (this.data.order.quantity > 1) {
+      this.data.order.quantity = this.data.order.quantity - 1;
+      this.setData({
+        order: this.data.order
+      })
+    }
 
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
---
+++ service/customerBox.js
@@ -0,0 +1,13 @@
+var wxe = require('../utils/wxe.js')
+module.exports = {
+    loadCustomerBoxItems: function (customer) {
+        console.log(customer);
+        return wxe.requestP({
+            url: 'customerBoxItem/example',
+            method: 'POST',
+            data: {
+                customer: customer
+            }
+        });
+    }
+}
