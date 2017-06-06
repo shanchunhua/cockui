e0846bf553d3a445d5fd06443bab0df32a3fa95a
shanch <shanch@cn.ibm.com>
2017-06-06 10:03:41 +0800

    detail

---
+++ pages/mycock/detail.js
@@ -0,0 +1,96 @@
+// index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+        background: ['green', 'red', 'yellow'],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: false,
+        interval: 3000,
+        duration: 1000
+  },
+  changeIndicatorDots: function (e) {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+  },
+  changeVertical: function (e) {
+      this.setData({
+          vertical: !this.data.vertical
+      })
+  },
+  changeAutoplay: function (e) {
+      this.setData({
+          autoplay: !this.data.autoplay
+      })
+  },
+  intervalChange: function (e) {
+      this.setData({
+          interval: e.detail.value
+      })
+  },
+  durationChange: function (e) {
+      this.setData({
+          duration: e.detail.value
+      })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
