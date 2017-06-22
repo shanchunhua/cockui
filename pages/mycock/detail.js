305f06de7656c1ef338462284735ed057f6714fb
shanch <shanch@cn.ibm.com>
2017-06-22 22:18:55 +0800

    0622

--- pages/mycock/detail.js
+++ pages/mycock/detail.js
@@ -17,7 +17,7 @@ Page({
     changeIndicatorDots: function (e) {
         this.setData({
             indicatorDots: !this.data.indicatorDots
-        })
+        });
     },
     changeVertical: function (e) {
         this.setData({
@@ -27,19 +27,27 @@ Page({
     changeAutoplay: function (e) {
         this.setData({
             autoplay: !this.data.autoplay
-        })
+        });
     },
     intervalChange: function (e) {
         this.setData({
             interval: e.detail.value
-        })
+        });
     },
     durationChange: function (e) {
         this.setData({
             duration: e.detail.value
-        })
+        });
     },
-
+    changeOrder: function (e) {
+        var current = e.detail.current;
+        console.log(current);
+        var order = this.data.orders[current];
+        this.setData({
+            currentOrder: order
+        });
+        console.log(order);
+    },
     /**
      * 生命周期函数--监听页面加载
      */
@@ -47,7 +55,9 @@ Page({
         var self = this;
         cockAdoptionOrderService.getMyRaisingOrdersByHennery(options.id).then(function (res) {
             self.setData({
-                orders: res.data
+                henneryId:options.id,
+                orders: res.data,
+                currentOrder: res.data[0]
             });
         });
     },
--- pages/mycock/withdraw.js
+++ pages/mycock/withdraw.js
@@ -1,66 +1,98 @@
 // pages/suborder2/index.js
+var app = getApp();
+var cockAdoptionOrderService = require('../../service/cockAdoptionOrder.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    withdrawOrder: {
+      quantity: 1,
+      price:209,
+      total:209
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var id = options.id;
+    console.log(id);
+    var self = this;
+    cockAdoptionOrderService.getById(id).then(function (res) {
+      self.setData({
+        order: res.data
+      });
+    });
   },
+  plus: function () {
+    if (this.data.withdrawOrder.quantity < this.data.order.quantity) {
+      this.data.withdrawOrder.quantity = this.data.withdrawOrder.quantity + 1;
+      this.data.withdrawOrder.total = this.data.withdrawOrder.quantity * this.data.withdrawOrder.price;
+      this.setData({
+        withdrawOrder: this.data.withdrawOrder
+      });
+    }
 
+  },
+  minus: function () {
+    if (this.data.withdrawOrder.quantity > 1) {
+      this.data.withdrawOrder.quantity = this.data.withdrawOrder.quantity - 1;
+      this.data.withdrawOrder.total = this.data.withdrawOrder.quantity * this.data.withdrawOrder.price;
+      this.setData({
+        withdrawOrder: this.data.withdrawOrder
+      });
+    }
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
